b3c3e5c1f7fb6d1cf92762c4b1a34f6bb9a309fa
Brian Quinlan <b_quinny@hotmail.com>
2018-10-18 13:50:00 +0000

    added tasks dict in flasktodo.py

--- flasktodo.py
+++ flasktodo.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+next_id = 3
+
+tasks = {
+	1 : {
+		"id": 1,
+		"name": "get coffee",
+		"description": "none of that cheap stuff",
+		"is_urgent": False
+	},
+	2 : {
+		"id": 2,
+		"name": "get milk",
+		"description": "in a carton",
+		"is_urgent": True
+	},
+}
+
+next_id += 1
+
 @app.route("/")
 def show_hi():
     return render_template("index.html")
