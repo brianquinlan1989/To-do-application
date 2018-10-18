0d6bd06a9e2b65cdc80105e29a8beff242521535
Brian Quinlan <b_quinny@hotmail.com>
2018-10-18 15:58:19 +0000

    added the add function for a new item on the list

--- flasktodo.py
+++ flasktodo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -19,11 +19,30 @@ tasks = {
 	},
 }
 
-next_id += 1
+
 
 @app.route("/")
-def show_hi():
+def show_index():
     return render_template("index.html", tasks=tasks.values())
+    
+@app.route("/add", methods=["GET", "POST"])
+def show_form():
+    global next_id
+    if request.method =="POST":
+        new_item = {
+            "id": next_id,
+    		"name": request.form["add_todo"],
+    		"description": request.form["add_description"],
+    		"is_urgent": "is_urgent" in request.form
+        }
+        
+        tasks[next_id] = new_item
+        next_id += 1
+        return redirect("/")
+    else:
+        return render_template("todo_form.html")
+    
+
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080, debug=True)
